2f484c201c49ed99b7b12b5969d9505333679eed
Смутин Даниил <Dsmutin2003@yandex.ru>
2023-03-18 19:56:58 +0300

    Second commit to main line eleven

--- smytin.cpp
+++ smytin.cpp
@@ -8,7 +8,7 @@ string longest_common_substring(string str1, string str2) {
     int n2 = str2.length();
 
     int dp[n1+1][n2+1];
-    int max_length = 0;
+    int max_length = 0; // Second commit main
     int end_pos = 0;
 
     for (int i = 0; i <= n1; i++) {
